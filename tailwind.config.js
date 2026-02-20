2c264c93c4ee096d7eb765fe88fab4b58ed4bc5d
Vipin A <vipins.mahe@gmail.com>
2026-02-21 00:18:33 +0530

    local project backup

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
